51720783c0f0b27b6aff3e858240f3c34e885192
Angus Freudenberg <freudenberg.angus@gmail.com>
2018-04-09 19:58:54 +1000

    change synchronise_playlists to point to internal storage to improve playlist compatibility with music players change disconnect_device to use adb kill-server rather than adb disconnect add line to get source directory of script reformat script layout

--- android-sync.sh
+++ android-sync.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# PATH definitions
-PATH=$PATH:/bin:/usr/bin:/usr/local/bin
-export PATH
-
 __TITLE__="Android Sync"
 __DESCRIPTION__="iTunes music synchronisation utility for android devices"
 __AUTHOR__="Angus Freudenberg, 2018"
@@ -12,13 +8,32 @@ __VERSION__="0.2.0b"
 echo "$__TITLE__ - $__VERSION__\n$__DESCRIPTION__\n$__AUTHOR__"
 echo
 
+# PATH definitions
+SOURCE="$( cd "$(dirname "$0")" ; pwd -P )"
+PATH=$PATH:/bin:/usr/bin:/usr/local/bin
+export PATH
+
 # Default iTunes library location
 HOST_MUSIC_PATH="$HOME/Music/iTunes/iTunes Media/Music"
+DEVICE_PLAYLIST_PATH=storage/emulated/0
+
+adb_check()
+{
+    # Try to launch adb to confirm it is present on the system
+
+    cmd=$(adb help 2>&1) || {
+        echo >&2 "ERROR: adb not functional, make sure it is in your PATH:"
+        echo $PATH
+        exit 1
+        }
+}
 
 check_device_connection()
 {
     # Check if a device is connected via USB
+
     echo "Checking if your Android device is connected..."
+    cmd=$(adb start-server 2>&1)
     device_state=$(adb get-state 2>&1)
         if [ "$device_state" != "device" ]
             then
@@ -28,21 +43,22 @@ check_device_connection()
 
     device=$(adb get-serialno)
     echo ${device} "is connected, finding external storage..."
-
 }
 
-# Terminate adb session
 disconnect_device()
 {
-    cmd=$(adb disconnect 2>&1)
+    # Kill the adb service
+
+    cmd=$(adb kill-server 2>&1)
     echo "It is now safe to unplug your device..."
     exit 1
 }
 
-# Try to get the UUID of the sd_card mounted in /storage and create a Music folder if it doesn't exists.
-# TODO Default to internal storage if an external storage device isn't present
 get_storage_path()
 {
+    # Try to get the UUID of the sd_card mounted in /storage and create a Music folder if it doesn't exists.
+    # TODO Default to internal storage if an external storage device isn't present
+
     sd_name=$(adb shell "cd storage/????-????; pwd | sed 's#.*/##'") ||
     {
         echo 'ERROR: There was a problem accessing the external storage device. Disengaging...'
@@ -56,23 +72,25 @@ get_storage_path()
     device_music_path=/storage/${sd_name}/Music
 }
 
-#TODO - Determine how to sync music files that have added/updated lyrics
 synchronise_music()
 {
     # Synchronise music from iTunes library to device sd card
+    #TODO - Determine how to sync music files that have added/updated lyrics
+
     echo "Synchronising music...."
-    adb-sync --dry-run "${HOST_MUSIC_PATH}/" "${device_music_path}" ||
+    adb-sync -f -d "${HOST_MUSIC_PATH}/" "${device_music_path}" ||
     {
         echo "ERROR: A problem occurred while transferring your music. Disengaging..."
         disconnect_device
     }
     echo
-
 }
 
 synchronise_playlists()
 {
     # Synchronise playlists from iTunes library to device sd card
+    #TODO - Switch directory to playlist file on internal storage for better compatibility with music players
+
     echo "Generating temporary directory..."
     playlist=$(mktemp -d)
     echo "Extracting playlists to temporary directory..."
@@ -84,7 +102,9 @@ synchronise_playlists()
     }
     echo
     echo "Synchronising playlists..."
-    adb-sync --dry-run "${playlist}/" "${device_music_path}" ||
+    # adb-sync --dry-run "${playlist}/" "${device_music_path}" ||
+    cmd=$(adb shell "cd ${DEVICE_PLAYLIST_PATH}; mkdir -p Playlists")
+    adb-sync -f -d "${playlist}/" "${DEVICE_PLAYLIST_PATH}/Playlists" ||
     {
         echo "A problem occurred while transferring your playlists. Disengaging..."
         disconnect_device
@@ -94,6 +114,8 @@ synchronise_playlists()
 
 clean_up_files()
 {
+    # Remove temporary files and redundant .DS_Store files from music folder
+
     echo "Cleaning up..."
     rm -rf ${playlist}
     cmd=$(adb shell "cd storage/${sd_name}/Music/; find . -name '.DS_Store' -delete")
@@ -101,16 +123,9 @@ clean_up_files()
     echo
 }
 
+# Main
 
-# MAIN
-# Check if adb is present and functional on the host system
-check_adb=$(adb help 2>&1) ||
-{
-    echo >&2 "ERROR: adb not functional, make sure it is in your PATH:"
-    echo $PATH
-    exit 1
-}
-
+adb_check
 check_device_connection
 get_storage_path
 synchronise_music
